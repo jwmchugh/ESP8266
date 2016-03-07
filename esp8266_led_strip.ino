7680b59eaba4a5f1137f7fad8f3b96b8617e47a7
jwmchugh <jwmchugh@gmail.com>
2016-03-07 19:33:06 +0000

    Update esp8266_led_strip.ino

--- esp8266_led_strip.ino
+++ esp8266_led_strip.ino
@@ -23,26 +23,14 @@ const short MIN_VIS_THRES = 100;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+// All light variables are in order R,G,B
+
 // Light state variables
 bool powered_on = false;
-bool red_on = false;
-bool green_on = false;
-bool blue_on = false;
-
-int red_brightness = MAX_LED_BRI;
-int blue_brightness = MAX_LED_BRI;
-int green_brightness = MAX_LED_BRI;
-
-int prev_red_brightness = red_brightness;
-int prev_blue_brightness = blue_brightness;
-int prev_green_brightness = green_brightness;
-bool prev_red_on = false;
-bool prev_green_on = false;
-bool prev_blue_on = false;
-
-
-
-
+bool light_on[3];
+bool prev_light_on[3];
+int light_brightness[3];
+int prev_light_brightness[3];
 
 // ESP8266 Data
 const int REDPIN = 12;
@@ -50,17 +38,16 @@ const int GREENPIN = 13;
 const int BLUEPIN = 16;
 
 void save_previous_state(){
-  prev_red_brightness = red_brightness;
-  prev_blue_brightness = blue_brightness;
-  prev_green_brightness = green_brightness;
+  for (int i=0;i<3;i++){
+    prev_light_brightness[0] = light_brightness[0];
+  }
 }
-
 
 // MQTT Callback function
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived on Topic[");
   Serial.print(topic);
-  Serial.print("Msg :");
+  Serial.print("] with message :");
   char* message = new char[length+1];
   for (int i=0;i<length;i++) {
     Serial.print((char)payload[i]);
@@ -68,161 +55,137 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   message[length] = '\0';
   if (strcmp(message, "fullpower") == 0){
-      green_brightness = red_brightness = blue_brightness = MAX_LED_BRI;
-      green_on = red_on = blue_on = true;
-      analogWrite(REDPIN, red_brightness);
-      analogWrite(GREENPIN, green_brightness);
-      analogWrite(BLUEPIN, blue_brightness);  
+    for (int i=0;i<3;i++){
+      light_brightness[i] = MAX_LED_BRI;
+      light_on[i] = true;
+    }
+    analogWrite(REDPIN, light_brightness[0]);
+    analogWrite(GREENPIN, light_brightness[1]);
+    analogWrite(BLUEPIN, light_brightness[2]);  
   }
   else if (strcmp(message, "halfpower") == 0){
-      green_brightness = red_brightness = blue_brightness = MAX_LED_BRI / 2;
-      green_on = red_on = blue_on = true;// clean
-      analogWrite(REDPIN, red_brightness);
-      analogWrite(GREENPIN, green_brightness);
-      analogWrite(BLUEPIN, blue_brightness);  
+    for (int i=0;i<3;i++){
+      light_brightness[i] = MAX_LED_BRI / 2;
+      light_on[i] = true;
+    }
+    analogWrite(REDPIN, light_brightness[0]);
+    analogWrite(GREENPIN, light_brightness[1]);
+    analogWrite(BLUEPIN, light_brightness[2]);  
   }
   else if (strcmp(message, "power") == 0){
-    if(green_on || red_on || blue_on )
+    if(light_on[0] || light_on[1] || light_on[2] )
     {
-      prev_red_on = red_on;
-      prev_blue_on = blue_on;
-      prev_green_on = green_on;
-      Serial.println("During power switch, a light was on. Switching all off now");
-      green_on = red_on = blue_on = false;
-      prev_red_brightness = red_brightness;
-      prev_green_brightness = green_brightness;
-      prev_blue_brightness = blue_brightness;
+      for(int i=0;i<3;i++){
+        prev_light_on[i] = light_on[i];
+        light_on[i] = false;
+        prev_light_brightness[i] = light_brightness[i];
+      }
       analogWrite(REDPIN, 0);
       analogWrite(GREENPIN, 0);
       analogWrite(BLUEPIN, 0);
     }
     else { // not powered_on
-      Serial.println("Ok, not powered on currently");
-      if (prev_red_on){
-        red_on = true;
-        red_brightness = prev_red_brightness;
-        analogWrite(REDPIN, red_brightness);
+      for (int i=0;i<3;i++)
+      {
+        if (prev_light_on[i]){
+          light_on[i] = true;
+          light_brightness[i] = prev_light_brightness[i]; 
+        }
       }
-      if (prev_blue_on){
-        blue_on = true;
-        blue_brightness = prev_blue_brightness;
-        analogWrite(BLUEPIN, blue_brightness);
+      if(prev_light_on[0]){
+        analogWrite(REDPIN, light_brightness[0]);
       }
-      if (prev_green_on){
-        green_on = true;
-        green_brightness = prev_green_brightness;
-        analogWrite(GREENPIN, green_brightness);
+      if(prev_light_on[1]){
+        analogWrite(GREENPIN, light_brightness[1]);
       }
-
-      if (blue_brightness < MIN_VIS_THRES && green_brightness < MIN_VIS_THRES && red_brightness < MIN_VIS_THRES){
+      if(prev_light_on[2]){
+        analogWrite(BLUEPIN, light_brightness[2]);
+      }
+      
+      if (light_brightness[0] < MIN_VIS_THRES && light_brightness[1] < MIN_VIS_THRES && light_brightness[2] < MIN_VIS_THRES){
         // Turn red for 1 second to notify brightness of all set to zero
         analogWrite(REDPIN, MAX_LED_BRI);
         delay(1000);
-        analogWrite(REDPIN, 0);       
+        analogWrite(REDPIN, 0);
+        for (int i=0;i<3;i++)
+        {
+          light_brightness[i] = MAX_LED_BRI / 2;
+        }
       }
     }
   }    
   else if (strcmp(message, "red") == 0){
-    if (red_on){
+    if (light_on[0]){
       analogWrite(REDPIN, 0);
     }
     else{
-      analogWrite(REDPIN, red_brightness);  
+      analogWrite(REDPIN, light_brightness[0]);  
     }
-    red_on = !red_on; 
-    Serial.print("Red on is : ");
-    Serial.println(red_on);
+    light_on[0] = !light_on[0]; 
   }
   else if (strcmp(message, "green") == 0){
-    if (green_on){
+    if (light_on[1]){
       analogWrite(GREENPIN, 0);
     }
     else{
-      analogWrite(GREENPIN, green_brightness);  
+      analogWrite(GREENPIN, light_brightness[1]);  
     }
-    green_on = !green_on; 
-    Serial.print("Green on is : ");
-    Serial.println(green_on);
+    light_on[1] = !light_on[1]; 
   }
   else if (strcmp(message, "blue") == 0){
-    if (blue_on){
+    if (light_on[2]){
       analogWrite(BLUEPIN, 0);
     }
     else{
-      analogWrite(BLUEPIN, blue_brightness);  
+      analogWrite(BLUEPIN, light_brightness[2]);  
     }
-    blue_on = !blue_on; 
-    Serial.print("blue on is : ");
-    Serial.println(blue_on);
+    light_on[2] = !light_on[2]; 
   }
-  else if (strcmp(message, "bright") == 0){
-    if (red_on){
-       if(red_brightness < MIN_VIS_THRES){
-         red_brightness = MIN_VIS_THRES;
-       }
-       red_brightness += red_brightness * 0.1;
-       if (red_brightness > MAX_LED_BRI){
-         red_brightness = MAX_LED_BRI;
-       }
-       Serial.print("Red Brightness is now: ");
-       Serial.print(red_brightness);
-       analogWrite(REDPIN, red_brightness);   
+  else if (strcmp(message, "brighter") == 0){
+    for (int i=0;i<3;i++){
+      if (light_on[i]){
+        light_brightness[i] += light_brightness[i] * 0.1;
+        if (light_brightness[i] > MAX_LED_BRI){
+          light_brightness[i] = MAX_LED_BRI;
+        }
+      }
     }
-    if (blue_on){
-       if(blue_brightness < MIN_VIS_THRES){
-         blue_brightness = MIN_VIS_THRES;
-       }
-       blue_brightness += blue_brightness * 0.1;
-       if (blue_brightness > MAX_LED_BRI){
-         blue_brightness = MAX_LED_BRI;
-       }
-       Serial.print("Blue Brightness is now: ");
-       Serial.print(blue_brightness);
-       analogWrite(BLUEPIN, blue_brightness);   
+    if (light_on[0]){
+      analogWrite(REDPIN, light_brightness[0]);   
     }
-    if (green_on){
-       if(green_brightness < MIN_VIS_THRES){
-         green_brightness = MIN_VIS_THRES;
-       }
-       green_brightness += green_brightness * 0.1;
-       if (green_brightness > MAX_LED_BRI){
-         green_brightness = MAX_LED_BRI;
-       }
-       Serial.print("Green Brightness is now: ");
-       Serial.print(green_brightness);
-       analogWrite(GREENPIN, green_brightness);   
+    if (light_on[1]){
+      analogWrite(GREENPIN, light_brightness[1]);   
+    }
+    if (light_on[2]){
+      analogWrite(BLUEPIN, light_brightness[2]);   
     }
   }
   else if (strcmp(message, "dim") == 0){
-    if (red_on){
-      red_brightness -= red_brightness * 0.1;
-      if (red_brightness < MIN_VIS_THRES){
-        red_brightness = 0;
-        red_on = false;
-      }
-      Serial.print("Red Brightness is now: ");
-      Serial.print(red_brightness);
-      analogWrite(REDPIN, red_brightness);   
+    for (int i=0;i<3;i++){
+      if (light_on[i]){
+        light_brightness[i] -= light_brightness[i] * 0.1;
+        if (light_brightness[i] < MIN_VIS_THRES){
+          light_brightness[i] = 0;
+        }
+      }      
     }
-    if (blue_on){
-      blue_brightness -= blue_brightness * 0.1;
-      if(blue_brightness < MIN_VIS_THRES){
-        blue_brightness = 0;
-        blue_on = false;
-      }
-      Serial.print("Blue Brightness is now: ");
-      Serial.print(blue_brightness);
-      analogWrite(BLUEPIN, blue_brightness);   
+    if (light_on[0]){
+      analogWrite(REDPIN, light_brightness[0]);   
+      if(light_brightness[0] == 0){
+        light_on[0] = false;
+      }        
     }
-    if (green_on){
-      green_brightness -= green_brightness * 0.1;
-      if(green_brightness < MIN_VIS_THRES){
-        green_brightness = 0;
-        green_on = false;
+    if (light_on[1]){
+      analogWrite(GREENPIN, light_brightness[1]);   
+      if(light_brightness[1] == 0){
+        light_on[1] = false;
       }
-      Serial.print("Green Brightness is now: ");
-      Serial.print(green_brightness);
-      analogWrite(GREENPIN, green_brightness);   
+    }
+    if (light_on[2]){
+      analogWrite(BLUEPIN, light_brightness[2]);   
+      if(light_brightness[2] == 0){
+        light_on[2] = false;
+      }
     } 
   }  // End Dim
   Serial.println();
@@ -230,7 +193,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 }
 
 void reconnect() {
-  // Loop until we're reconnected
+  //   Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
@@ -288,6 +251,13 @@ void setup()
   digitalWrite(GREENPIN, LOW);
   digitalWrite(BLUEPIN, LOW);
   delay(300);
+
+  // Initalise variables
+  for (int i=0;i<3;i++){
+    light_on[i] = false;
+    light_brightness[i] = MAX_LED_BRI;
+    prev_light_brightness[i] = light_brightness[i];
+  }
 }
 
 void loop()
@@ -297,6 +267,5 @@ void loop()
   }
   client.loop();
 }  
-
 
 
